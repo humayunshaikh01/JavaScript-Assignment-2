aa54df14a249936f765fd5c23ce7fc380766949b
humayunshaikh01 <shaikhhumayun99@gmail.com>
2023-02-15 03:08:11 +0500

    jsAssigment

---
+++ app.js
@@ -0,0 +1,235 @@
+// 
+
+// Marksheet Practice
+
+// var Percentage = +prompt("Enter Your Percentage");
+
+// if (Percentage >= 80 && Percentage <= 100) {
+//     document.write("A+")
+// } else if (Percentage >= 70 && Percentage <= 79) {
+//     document.write("A")
+// } else if (Percentage >= 60 && Percentage <= 69) {
+//     document.write("B")
+// } else if (Percentage >= 50 && Percentage <= 59) {
+//     document.write("C")
+// } else if (Percentage >= 40 && Percentage <= 49) {
+//     document.write("D")
+// } else if (Percentage >= 0 && Percentage <= 39) {
+//     document.write("Fail")
+// } else {
+//     document.write("invalid number")
+// }
+
+
+
+
+
+// Assignment 2
+
+
+// // Chapter No, 1
+
+
+// // Question # 1
+// alert("wellcome to my page");
+
+// // Question # 2
+// alert("Error! Please enter a valid password.");
+
+// // Question # 3
+// alert("Welcome to js Land \n Happy Coding!");
+
+// // Question # 4
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+// // Question # 5
+// alert("Hello... I can run JS through my web browser's console");
+
+
+
+
+
+// Chapter No 2
+
+
+
+
+
+// // Question # 1
+// var userName
+
+// // Question # 2
+// var userName = ("Jhone Doe");
+// alert(userName);
+
+// // Question # 3
+// var userAge = ("15 Years Old");
+// alert(userAge);
+
+// // Question # 4
+// var userInfo = ("Certified Mobile Application Development");
+// alert(userInfo);
+
+// // Question # 5
+// var Pizza = ("PIZZA \n PIZZ \n PIZ \n PI \n P");
+// alert(Pizza);
+
+// // Question # 6
+// var emailAddress = ("Myemail@gmail.com");
+// alert(emailAddress);
+
+// // Question # 7
+// var book = ("A Smarter way to learn JavaScript");
+// alert(book);
+
+// // Question # 8
+// var text = ("Yah! I can write HTML content through JavaScript");
+// document.write(text);
+
+// // Question # 9
+// var style = ("-------&%#-------");
+// alert(style);
+// document.write(style);
+
+
+// Chapter No 3
+
+
+// Question # 1
+// var userAge = ("I'm 24 Years old.");
+// alert(userAge);
+
+// // Question # 2
+// var visitCount = ("You have visited this site 14 times");
+// alert(visitCount);
+
+// // Question # 3
+// var birthYear = ("My birth Year is 1998 \n Data type of variable is number");
+// document.write(birthYear);
+
+// // Question # 4
+// var store = ("John Doe ordered 5 T-Shirts(s) on XYZ Store");
+// document.write(store);
+
+
+// Chapter No 4
+
+
+// // Question # 1
+
+// var ans1 = ("variable names can only contain name,Numbers, $, _, ex. myName, 10, $name, _name");
+// var ans2 = ("variable must begin with these, _,$, ex. _name, $name");
+// var ans3 = ("variable names ared case sensitive");
+// var ans4 = ("variable names should not be JS keywords");
+
+// document.write(ans1 + ans2 + ans3 + ans4);
+// // document.write(ans2);
+// // document.write(ans3);
+// // document.write(ans4);
+
+
+// Chapter No 5
+
+
+
+// Question # 1
+    // Add
+    // var num1 = 8;
+    // var num2 = 18;
+    // var num3 =  num1 + num2;
+    // document.write(num3);
+
+
+// Question #2
+
+    // Sub
+    // var num1 = 8;
+    // var num2 = 18;
+    // var num3 = num1 - num2;
+    // document.write(num3);
+
+    // // Mul
+    // var num1 = 8;
+    // var num2 = 18;
+    // var num3 = num1 * num2;
+    // document.write(num3);
+
+    // div
+
+    // var num1 = 8;
+    // var num2 = 18;
+    // var num3 = num1 / num2;
+    // document.write(num3);
+
+    // modu
+    // var num1 = 30;
+    // var num2 = 9;
+    // var num3 = num1 % num2;
+    // document.write(num3);
+
+
+// Question #3
+
+    // var a = 5;       DOUBT
+    // a++ + 1;
+    // var b = 7;
+    // --b;
+    // var c = 12 % 3;
+    // // document.write(a + b);
+    // document.write(c);
+
+// Question # 4
+
+    // var movieTickets = 600;
+    // var buyers = 5;
+    // var movie = (buyers * movieTickets);
+
+    // document.write(movie);
+
+// Question # 5
+
+
+// Question # 6
+
+
+// Question # 7
+
+
+// Question # 8
+
+    // var oM = 516;
+    // var tM = 850;
+    // var mul = 100;
+    // var marks = (oM * mul / tM);
+    // document.write(marks);
+
+// Question # 9
+
+    // var usd1 = 104.80;
+    // var sr1 = 28;
+    // var total = usd1 * 10 + sr1 * 25;
+    // document.write(total);
+
+// Question # 10
+
+    // var num1 = 5;
+    // var num2 = 10;
+    // var num3 = 2;
+    // var total = (num1 * num2 / num3);
+
+    // document.write(total);
+
+
+// Question # 11
+
+    // var cY = 2023;
+    // var bY = 1998;
+    // var total = (cY - bY);
+    // document.write(total);
+
+
+// Question # 12
+
+
+
+// Question # 13
