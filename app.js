3182ed6184e468451efb6ae9c35a8ea34bd7c922
humayunshaikh01 <shaikhhumayun99@gmail.com>
2023-02-17 14:52:50 +0500

    added some work (JS Assignment 2)

--- app.js
+++ app.js
@@ -194,6 +194,15 @@
 
 // Question # 7
 
+    //var item1 = 650;
+    //var qty1 = 3;
+    //var item2 = 100;
+    //var qty2 = 7;
+    //var COD = 100;
+    //var shopTotal = (650 * 3 + 100 * 7 + 100);
+    //document.write(shopTotal);
+
+
 
 // Question # 8
 
@@ -233,3 +242,310 @@
 
 
 // Question # 13
+
+    //var snack = "Chip";
+    //var currentAge = 15;
+    //var estMin = 65;
+    //var snackPerDay = 3;
+
+    //var snacks = (65 - 15) * 3;
+
+    //document.write(snacks);
+
+// Chapter No 6/9
+
+// Question # 1
+
+    // completed in html
+
+// Question # 2
+
+    // var a = 2 , b = 1;
+
+    // var result = --a - --b + ++b + b--;
+
+    // result = 1 - 0 + 1 + 0;
+    // result = 2;
+
+    // document.write("Q2) Answer is 2 <br />");
+
+// Question # 3
+
+    // var userName = prompt("Enter Your Name");
+    // document.write("Q3)" + userName);
+
+// Question # 4/5
+
+    // var userInput = +prompt("Enter Number")
+
+
+
+// Question # 6
+
+// var subName1 = prompt("Enter your 1st Subject Name");
+
+// document.write(subName1 );
+
+// var totalMarks1 = ("100" + " ");
+// document.write(totalMarks1);
+
+// var obtMarks1 = +prompt("Enter 1st Subject Optain Marks" );
+
+// document.write(obtMarks1 + " ");
+
+// var percentage1 = ("54%" + " " );
+// document.write(percentage1 + "<br>")
+
+
+// var subName2 = prompt("Enter your 2nd Subject Name")
+// document.write(subName2);
+
+// var totalMarks2 = ("100" + " ") ;
+// document.write(totalMarks2 );
+
+// var obtMarks2 = +prompt("Enter 2nd Subject Obtain Marks");
+
+// document.write(obtMarks2 + " ");
+
+// var percentage2 = ("54" + " ");
+// document.write(percentage1 + "<br>")
+
+
+// var subName3 = prompt("Enter your 3rd Subject Name");
+
+// document.write(subName3);
+
+// var totalMarks3 = ("100" + " ") ;
+// document.write(totalMarks3 );
+
+// var obtMarks3 = +prompt("Enter 3rd Subject Optain Marks");
+// document.write(obtMarks3 +" ");
+
+// var percentage3 = ("48" + " ");
+// document.write(percentage1 + "<br> <br>");
+
+// let totalMarks = 300;
+// document.write("300" + " ");
+
+// let optMarks = 156;
+// document.write("156" + " ");
+
+// let percentage = 52;
+// document.write("52%" + " ");
+
+
+
+// chapter 9/11
+
+// Question 1
+
+// var city = prompt("Enter any City Name");
+// var cityName = "karachi" 
+// if(cityName == city){
+//     document.write("WellCome to the city of lights...");
+// }else{
+//     document.write("WellCome to the beautiful city!");
+// }
+
+// Question 2
+
+// var userGender = prompt("Enter Gender");
+// var gender = "male";
+// var gender2 = "female";
+
+// if(userGender == gender){
+//     document.write("Good Morning Sir!")
+// }else if(userGender == gender2){
+//     document.write("Good Morning Ma'am!")
+// }else{
+//     document.write("User not identified")
+// }
+    
+
+// Question No 4
+
+// . Write a program to take input remaining fuel in car (in 
+//     litres) from user. If the current fuel is less than 0.25litres, 
+//     show the message “Please refill the fuel in your car
+
+// let remainingFuel = +prompt("enter the remaining fuel of your car");
+
+// if(remainingFuel < 0.25){
+// document.write("Please refill  the fuel in your car")
+// }else{
+//     document.write("you can drive longer ")
+// }
+
+
+// Question 5
+
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// Displayed
+
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// not displayed
+
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// // not displayed
+
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// // displayed
+
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// // not displayed
+
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// displayed
+
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// displayed
+//  if (true){
+// alert("True");
+// }
+// display true
+// if (false){
+// alert("False");
+// }
+// display true
+
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+// displayed
+
+
+
+// Question No 6
+
+// var Maths = +prompt("Enter Your Maths obtain marks From 1 to 100");
+// var English = +prompt("Enter Your English obtain marks From 1 to 100");
+// var Urdu = +prompt("Enter Your Urdu obtain marks From 1 to 100");
+
+// var totalMarks = 300;
+// var obtainMarks = Maths + English + Urdu;
+// var percentage = (obtainMarks / totalMarks) * 100;
+
+// if(percentage >= 80){
+//     document.write("Total Marks " + totalMarks+ `<br>`)
+//     document.write("Obtain Marks " + obtainMarks+ `<br>`)
+//     document.write("Percentage " + percentage+"%"+ `<br>`);
+//     document.write("Grade: A-one " +`<br>`);
+//     document.write("Remarks: Excellent ");
+// }else if(percentage >= 70){
+//     document.write("Total Marks " + totalMarks+ `<br>`)
+//     document.write("Obtain Marks " + obtainMarks+ `<br>`)
+//     document.write("Percentage " + percentage+"%"+ `<br>`);
+//     document.write("Grade: A " +`<br>`);
+//     document.write("Remarks: Good ");
+// }else if(percentage >= 60){
+//     document.write("Total Marks " + totalMarks+ `<br>`)
+//     document.write("Obtain Marks " + obtainMarks+ `<br>`)
+//     document.write("Percentage " + percentage+"%"+ `<br>`);
+//     document.write("Grade: B "+`<br>`);
+//     document.write("Remarks: You Need to Improve ");
+// }else{
+//     document.write("Total Marks " + totalMarks+ `<br>`)
+//     document.write("Obtain Marks " + obtainMarks+ `<br>`)
+//     document.write("Percentage " + percentage+"%"+ `<br>`);
+//     document.write("Grade: Fail "+`<br>`);
+//     document.write("Remarks: Sorry ");
+// }
+
+// Question 7
+
+// var guessNum = +prompt("Guess the Number from 1 to 15");
+// var secretNum = 6;
+
+// if(guessNum === secretNum){
+//     document.write("Bingo! correct answer")
+// }else if(guessNum === 5 || guessNum === 7){
+//     document.write("Close enough to correct answer")
+// }else{
+//     document.write("Try Again!")
+// }
+
+// Question 8
+
+// var userNum = +prompt("Please enter any number.. I'll tell you that the number is divisible or not");
+// var divNum =  userNum % 3;
+// if(divNum == 0){
+//     document.write("User Number is divisible by 3")
+// }else{
+//     document.write("User Number is not divisible by 3")
+// }
+
+// Question 9
+
+// var userNum = +prompt("Please enter any number.. I'll tell you that the number is divisible or not");
+// var divNum =  userNum % 2;
+// if(divNum == 0){
+//     document.write(userNum + " " + "is even")
+// }else{
+//     document.write(userNum + " " + "is odd")
+// }
+
+// Question 10
+
+// var temp = +prompt("What is the Temperature")
+
+
+
+// Chapter 12/13
+
+//Question 3
+
+// var userNum = +prompt("Enter any Number");
+// var myNum = userNum;
+
+// if(userNum >= 1){
+//   document.write("This is a Positive Number");
+// }
+//   else if(userNum == 0){
+//     document.write("This is a Zero")
+//   }
+//   else{
+//     document.write("This is a negative Number")
+//   }
+
+
+//Question 5
+
+//var userpassword = prompt("Enter Password");
+
+//var correctPassword= "12345pak"
+
+//if(userpassword === correctPassword){
+//  alert("Correct Password");
+//}
+//else if(userpassword == ""){
+//  alert('Enter password');
+//}
+
+//else if(userpassword !== correctPassword){
+//};
+
+//else{
+// alert("Enter password");
+//};
+
+
